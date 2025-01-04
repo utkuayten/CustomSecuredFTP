0d9223e45ad7a5882dc86e7c33602954bf78d95e
erdemsezen <erdemsezen03@gmail.com>
2025-01-04 19:42:45 +0300

    son2

--- client_side/main.py
+++ client_side/main.py
@@ -1,16 +1,19 @@
 from ftplib import FTP
-import tkinter as tk
-
+from kivy.app import App
 from UI.LoginPanel import LoginPanel
 
 if __name__ == "__main__":
     # FTP connection details
     FTP_HOST = "16.170.206.200"  # Server address (use '127.0.0.1' for localhost)
-
     FTP_PORT = 2121  # Port number
 
-    root = tk.Tk()
+    # Initialize FTP connection
     ftp = FTP()
-    ftp.connect(FTP_HOST, FTP_PORT)
-    app = LoginPanel(root, ftp)
-    root.mainloop()
+    ftp.connect(FTP_HOST, FTP_PORT)  # Connect to the FTP server
+
+    # Create and run the Kivy app
+    class MainApp(App):
+        def build(self):
+            return LoginPanel(ftp=ftp)
+
+    MainApp().run()
