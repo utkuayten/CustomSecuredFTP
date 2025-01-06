5f38425cc337bc42a6a813668214e43124fcbb5a
berketuncerozu <147720933+berketuncerozu@users.noreply.github.com>
2025-01-07 02:30:50 +0300

    berke ve yigit kodu bastan yaratti

---
+++ client_side/UI/encryption.py
@@ -0,0 +1,40 @@
+
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+class RSACipher:
+    @staticmethod
+    def encrypt_key(public_key_pem, key):
+        """Encrypt the hash with the public key."""
+        public_key = serialization.load_pem_public_key(public_key_pem)
+        encrypted_key = public_key.encrypt(
+            key,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return encrypted_key
+
+    @staticmethod
+    def generate_keys():
+        """Generate RSA key pair."""
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+        )
+        public_key = private_key.public_key()
+
+        private_pem = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+
+        public_pem = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+        return private_pem, public_pem
