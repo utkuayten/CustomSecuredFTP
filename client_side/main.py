10e3371c4c28c76b0553d1fcb76051e7039d28f8
utku.ayten <103459721+utkuayten@users.noreply.github.com>
2025-01-07 03:34:12 +0300

    fix merge

--- client_side/main.py
+++ client_side/main.py
@@ -5,18 +5,16 @@ from client_side.UI.Hasher import Hasher
 from client_side.UI.encryption import RSACipher
 import os
 
-# AWS Sunucu Bilgileri
-FTP_HOST = "16.170.206.200"  # AWS sunucu IP adresi veya hostname
-FTP_PORT = 2121  # AWS sunucunun FTP portu
 
-    # Initialize FTP connection
-    ftp = FTP()
-    ftp.connect(FTP_HOST, FTP_PORT)  # Connect to the FTP server
+# Create and run the Kivy app
+class MainApp(App):
+    def build(self):
+        FTP_HOST = "16.170.206.200"
+        FTP_PORT = 2121
+        ftp = FTP()
+        ftp.connect(FTP_HOST, FTP_PORT)
+        return LoginPanel(ftp=ftp)
 
-    # Create and run the Kivy app
-    class MainApp(App):
-        def build(self):
-            return LoginPanel(ftp=ftp)
 
 if __name__ == "__main__":
     MainApp().run()
